a2e12dfe053a1fde450e3332b86dc0f0c5c337e0
viviane souza <vivianedasilvasouza2020@gmail.com>
2020-10-31 21:35:41 -0300

    lembrandopy

---
+++ imgs/cursopy/curso.py
@@ -0,0 +1,11 @@
+print('olá mundo!')
+nome=('viviane')
+idade=('26')
+peso=('68.0')
+print(nome,idade,peso)
+
+
+nome=input ('qual seu nome?') 
+idade=input ('qual sua idade?')
+peso=input ('quanto você pesa?')
+print(nome,idade,peso)
